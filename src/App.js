23492ca3bbb2ab075f691963fa9fc3e28affc377
Lise <lise.carpenter@gmail.com>
2021-04-29 15:03:34 +0700

    added JS example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import annapurna from './annapurna.jpg'
 import './App.css'
 
+function annapurnaSays() {
+	alert('Meow!')
+}
+
 function App() {
 	return (
 		<div className='App'>
 			<div>
-				<img src={annapurna} className='App-logo' alt='logo' />
+				<img
+					src={annapurna}
+					className='App-logo'
+					alt='logo'
+					onClick={() => annapurnaSays()}
+				/>
 				<p>
 					Hi! My name is <strong>Annapurna</strong> and I'm a persian cat.
 				</p>
